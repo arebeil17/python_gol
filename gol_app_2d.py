8d5b3da256e94dec3c11dea433d0fffc867e767e
arebeil17 <andres07@email.arizona.edu>
2021-11-09 21:43:29 -0700

    fixed bugs with cursor mode

--- gol_app_2d.py
+++ gol_app_2d.py
@@ -36,9 +36,11 @@ grid_dim = [cols, rows]
 game_state = GameState2D(
     game_grid, 0, FPS, display_surface, all_sprites, sprite_map)
 
-# game_config = GameConfig(cell_dim, grid_dim, window_dim, cell_color_mode)
+game_config = GameConfig(cell_dim, grid_dim, window_dim, cell_color_mode)
 
-# game_state.game_grid = grid.randomize_grid(game_state.game_grid, rows)
+game_state.game_grid = grid.randomize_grid(game_state.game_grid, rows)
+
+game_sprites.initiaize_grid_sprites(game_config, game_state)
 # ---------------------------------------------------------
 manager = pygame_gui.UIManager((window_dim[0], window_dim[1]), 'menu.json')
 
@@ -66,11 +68,11 @@ color_rect = pygame.Rect((window_dim[0] // 2 - 100,
                           window_dim[1] // 2 + 200), (200, 50))
 
 fps_setting = 1
-scale_setting = 3
+scale_setting = 2
 color_setting = 1
 fps_list = [["slow", 1], ["normal", 2], ["fast", 5], ["ultra", 10]]
-scale_list = [["micro", 2, 2], ["tiny", 5, 5], ["small", 10, 10],
-              ["normal", 20, 20], ["large", 40, 40]]
+scale_list = [["micro", 5, 5], ["small", 10, 10], [
+    "normal", 20, 20], ["large", 40, 40], ["Collosal", 80, 80]]
 color_list = [["green", 0], ["colorful", 1], ["disco", 2]]
 num_speeds = len(fps_list)
 num_scales = len(scale_list)
@@ -94,32 +96,35 @@ while is_running:
 
     time_delta = clock.tick(60)/1000.0
 
-    mouse_up_detected = False
+    setting_update = False
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             is_running = False
             print("Exiting game!")
             sys.exit(0)
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            mouse_up_detected = True
 
         if event.type == pygame.USEREVENT:
+
             if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
+
                 if event.ui_element == start_button:
                     print('start simulation')
                     board_init_mode = InitMode.Random
                     is_running = False
+
                 if event.ui_element == configure_button:
                     board_init_mode = InitMode.Cursor
                     print('configure mode')
                     is_running = False
+
                 if event.ui_element == speed_button:
                     fps_setting = (fps_setting + 1) % num_speeds
                     pygame_gui.elements.UIButton(speed_rect, text=f'Sim Speed: {fps_list[fps_setting][0]}',
                                                  manager=manager)
-                    FPS = fps_list[fps_setting][1]
+                    game_state.fps = fps_list[fps_setting][1]
                     print('Sim Speed: ', {fps_list[fps_setting][0]})
+
                 if event.ui_element == scale_button:
                     scale_setting = (scale_setting + 1) % num_scales
                     pygame_gui.elements.UIButton(scale_rect, text=f'Cell Size: {scale_list[scale_setting][0]}',
@@ -127,21 +132,35 @@ while is_running:
 
                     cell_dim = scale_list[scale_setting][1:]
                     print('Scale: ', {scale_list[scale_setting][0]})
+                    setting_update = True
+
                 if event.ui_element == color_button:
                     color_setting = (color_setting + 1) % num_colors
                     pygame_gui.elements.UIButton(color_rect, text=f'Color: {color_list[color_setting][0]}',
                                                  manager=manager)
 
-                    cell_color_mode = color_list[color_setting][1]
+                    game_config.color_mode = color_list[color_setting][1]
                     print('Color Mode: ', {color_list[color_setting][0]})
+                    setting_update = True
 
         manager.process_events(event)
 
     manager.update(time_delta)
 
+    if setting_update:
+        game_sprites.dynamic_grid_sprite_update(
+            game_config, game_state, cell_dim, window_dim)
+
     game_state.display_surface.blit(background, (0, 0))
-    game_state.display_surface.blit(text, text_rect)
+
+    game_state.all_sprites.update()
+    game_state.display_surface.fill((0, 0, 0))
+
+    for entity in game_state.all_sprites:
+        game_state.display_surface.blit(entity.surf, entity.rect)
+
     manager.draw_ui(game_state.display_surface)
+    game_state.display_surface.blit(text, text_rect)
 
     pygame.display.update()
 
@@ -155,13 +174,15 @@ cols = len(game_grid[0])
 grid_dim = [cols, rows]
 
 game_state = GameState2D(
-    game_grid, 0, FPS, display_surface, all_sprites, sprite_map)
+    game_grid, 0, game_state.fps, display_surface, all_sprites, sprite_map)
 
-game_config = GameConfig(cell_dim, grid_dim, window_dim, cell_color_mode)
+game_config = GameConfig(
+    cell_dim, grid_dim, window_dim, game_config.color_mode)
 
 game_sprites.initiaize_grid_sprites(game_config, game_state)
 
-if board_init_mode == InitMode.Random:
+
+if InitMode(board_init_mode) == InitMode.Random:
     game_state.game_grid = grid.randomize_grid(game_state.game_grid, rows)
 else:
     game_control.cursor_mode_2d(game_config, game_state)
--- gol_game_pkg/game_sprites.py
+++ gol_game_pkg/game_sprites.py
@@ -1,8 +1,11 @@
 import pygame
 from pygame import Color, Surface
+from pygame.locals import *
 import random
 from gol_game_pkg.game_constants import RGB_COLOR_DICT as rgb_dict
 from gol_game_pkg.game_constants import CellColor
+import gol_game_pkg.grid as grid
+from gol_game_pkg.game_types import GameConfig, GameState2D
 
 
 class CellSprite(pygame.sprite.Sprite):
@@ -48,3 +51,43 @@ def initiaize_grid_sprites(game_config, game_state):
             game_state.all_sprites.add(cell)
             # key = "x,y" and val = CellSprite object
             game_state.sprite_map[f'{x},{y}'] = cell
+
+
+def dynamic_grid_sprite_update(game_config, game_state, cell_dim, window_dim):
+
+    game_grid = grid.initialize_grid_from_window_size(cell_dim, window_dim)
+
+    rows = len(game_grid)
+    cols = len(game_grid[0])
+    grid_dim = [cols, rows]
+
+    game_state.all_sprites.empty()
+
+    game_state = GameState2D(
+        game_grid, 0, game_state.fps, game_state.display_surface, game_state.all_sprites, {})
+
+    game_config = GameConfig(
+        cell_dim, grid_dim, window_dim, game_config.color_mode)
+
+    game_state.game_grid = grid.randomize_grid(game_state.game_grid, rows)
+
+    initiaize_grid_sprites(game_config, game_state)
+
+
+def reinitialize_grid_sprites_to_dead(game_config, game_state, cell_dim, window_dim):
+
+    game_grid = grid.initialize_grid_from_window_size(cell_dim, window_dim)
+
+    rows = len(game_grid)
+    cols = len(game_grid[0])
+    grid_dim = [cols, rows]
+
+    game_state = GameState2D(
+        game_grid, 0, game_state.fps, game_state.display_surface, game_state.all_sprites, game_state.sprite_map)
+
+    game_config = GameConfig(
+        cell_dim, grid_dim, window_dim, game_config.color_mode)
+
+    initiaize_grid_sprites(game_config, game_state)
+
+    return game_state
