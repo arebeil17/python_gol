d6604161427dc4f0b7a9d01b2b966f03918c7d0c
arebeil17 <andres07@email.arizona.edu>
2021-11-20 15:40:13 -0700

    Removed deprecated CLI implementation

--- gol_app.py
+++ gol_app.py
@@ -12,9 +12,9 @@ game_gui = GameGui(game_config)
 game_control.run_main_menu(game_gui, game_state, game_config)
 
 if GameMode(game_config.get_current_selection("mode")[1]) == GameMode.Cursor:
-    
+
     game_sprites.reset_all_sprites_to_dead(game_state, game_config)
-    
+
     game_control.cursor_mode_2d(game_config, game_state)
 
 game_control.simualtion_mode_2d(game_gui, game_config, game_state)
